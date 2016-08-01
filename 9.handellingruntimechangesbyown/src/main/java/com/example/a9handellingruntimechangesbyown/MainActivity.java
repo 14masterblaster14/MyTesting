a796583d2bde4b19da3c21680ecc358e121c7327
comp <14masterblaster14@gmail.com>
2016-08-01 17:14:54 +0530

    Handeling Run Time changes for potrate/Landscape mode

--- 9.handellingruntimechangesbyown/src/main/java/com/example/a9handellingruntimechangesbyown/MainActivity.java
+++ 9.handellingruntimechangesbyown/src/main/java/com/example/a9handellingruntimechangesbyown/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.a9handellingruntimechangesbyown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.BtnSubmit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView textView = (TextView) findViewById(R.id.TxtTime);
+                textView.setText(" " + new Date());
+            }
+        });
+        showToast("onCreate");
+
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        showToast("onRestart");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        showToast("onStart");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        showToast("onResume");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        showToast("onPause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        showToast("onStop");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        showToast("onDestroy");
+    }
+
+    public void showToast(String msg) {
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
