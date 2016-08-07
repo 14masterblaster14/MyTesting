9566ce49c3928caf49c548556c210b535ad358d2
comp <14masterblaster14@gmail.com>
2016-08-07 17:22:02 +0530

    Dir Commit

---
+++ 20.sqlite/src/main/java/com/example/a18sqlite/MainActivity.java
@@ -0,0 +1,191 @@
+package com.example.a18sqlite;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DbHelper dbHelper = new DbHelper(this, "My.SqlDb", null, 1);
+        /* My.SqlDb --> Unique DB name for your application*/
+
+        findViewById(R.id.BtnInsert).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqlDb = dbHelper.getWritableDatabase();
+
+                String table = "UserTable";
+                String nullColumnHack = null;
+                /*nullColumnHack --> Specify the column name which can consist of null value.
+                                     Here null means every column should have some value */
+                ContentValues values = new ContentValues();
+                values.put("UserName", getUserName());
+                values.put("RollNo", getRollNo());
+                Log.i("sqlite", "UserName :" + getUserName());
+                Log.i("sqlite", "RollNo :" + getRollNo());
+                sqlDb.insert(table, nullColumnHack, values);
+
+                sqlDb.close();
+            }
+        });
+
+
+        findViewById(R.id.BtnQuery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqlDb = dbHelper.getReadableDatabase();
+
+                String table = "UserTable";
+                String[] columns = {"UserName"};
+                /* String[] columns = null; */
+                /* columns --> columns to be return from query and displayed.
+                               Null will return all columns.                    */
+
+                String selection = "rollNo = ?";
+                /* selection --> filter for which rows to return.
+                 *               Null will return all data i.e. Select * */
+
+                String[] selectionArgs = {getRollNo()};
+                /* selectionArgs --> It will replace the $ in selection clause
+                                    Null will return all data i.e. Select * */
+
+                String groupBy = null;
+                String having = null;
+                String orderBy = null;
+
+                Cursor cursor = sqlDb.query(table, columns, selection, selectionArgs, groupBy, having, orderBy);
+
+                while (cursor.moveToNext()) {
+
+                    String userName = cursor.getString(0);
+                    /*0--> Column index*/
+                    /* int rollNo = cursor.getInt(cursor.getColumnIndex("RollNo")); */
+
+                    Log.i("sqlite", "UserName : " + userName);
+
+                }
+
+                sqlDb.close();
+            }
+        });
+
+
+        findViewById(R.id.BtnUpdt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqlDb = dbHelper.getWritableDatabase();
+
+                String table = "UserTable";
+                ContentValues values = new ContentValues();
+                values.put("UserName", getUserName());
+                String whereClause = "RollNo = ? ";
+                String[] whereArgs = {getRollNo()};
+                sqlDb.update(table, values, whereClause, whereArgs);
+
+                sqlDb.close();
+            }
+        });
+
+        findViewById(R.id.BtnDelete).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                SQLiteDatabase sqlDb = dbHelper.getWritableDatabase();
+
+                String table = "UserTable";
+                String whereClause = "RollNo = ?";
+                String[] whereArgs = {getRollNo()};
+                sqlDb.delete(table, whereClause, whereArgs);
+                sqlDb.close();
+            }
+        });
+
+        findViewById(R.id.BtnShowTbl).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase sqlDb = dbHelper.getReadableDatabase();
+
+                String table = "UserTable";
+                /* String[] columns = {"UserName"}; */
+                String[] columns = null;
+                /* columns --> columns to be return from query and displayed.
+                               Null will return all columns.                    */
+
+                /* String selection = "rollNo = ?"; */
+                String selection = null;
+                /* selection --> filter for which rows to return.
+                 *               Null will return all data i.e. Select * */
+
+                /* String[] selectionArgs = {getRollNo()}; */
+                String[] selectionArgs = null;
+                /* selectionArgs --> It will replace the $ in selection clause
+                                    Null will return all data i.e. Select * */
+
+                String groupBy = null;
+                String having = null;
+                String orderBy = "RollNo";
+                /*  String orderBy = "RollNo Dsc"; */
+
+                Cursor cursor = sqlDb.query(table, columns, selection, selectionArgs, groupBy, having, orderBy);
+
+                ArrayList<String> dataset = new ArrayList<>();
+
+                while (cursor.moveToNext()) {
+
+                    String userName = cursor.getString(0);
+                    /*0--> Column index*/
+                    /* int rollNo = cursor.getInt(cursor.getColumnIndex("RollNo")); */
+
+                    int rollno = cursor.getInt(1);
+
+                    Log.i("sqlite", "UserName --> " + userName);
+                    Log.i("sqlite", "RollNo --> " + rollno);
+
+                    dataset.add(userName + " --> " + rollno);
+
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, dataset);
+
+                ((ListView) findViewById(R.id.LstView)).setAdapter(adapter);
+
+                sqlDb.close();
+
+            }
+        });
+    }
+
+
+    private String getUserName() {
+
+        return ((EditText) findViewById(R.id.EdtUsrName))
+                .getText()
+                .toString();
+    }
+
+    private String getRollNo() {
+
+        return ((EditText) findViewById(R.id.EdtRollNo))
+                .getText()
+                .toString();
+    }
+
+}
