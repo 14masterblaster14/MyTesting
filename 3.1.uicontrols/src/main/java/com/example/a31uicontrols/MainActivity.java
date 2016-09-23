0b5872b2720a77799615ef08c8ae388febceb181
comp <14masterblaster14@gmail.com>
2016-09-23 08:28:31 +0530

    Dir Commit

--- 3.1.uicontrols/src/main/java/com/example/a31uicontrols/MainActivity.java
+++ 3.1.uicontrols/src/main/java/com/example/a31uicontrols/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,12 +15,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    EditText editText = (EditText) findViewById(R.id.EdtTxt);
-    String edittextinput = editText.getText().toString();
-    TextView textView = (TextView) findViewById(R.id.TxtView);
-
-    // My Code starts here -->>
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // My Code starts here -->>
+
+        EditText editText = (EditText) findViewById(R.id.EdtTxt);
+        final String edittextinput = editText.getText().toString();
+        Log.v("UIControl", "edittextinput :" + edittextinput);
+
+        findViewById(R.id.Btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView autoTextView = (TextView) findViewById(R.id.AutoTxtView);
+                autoTextView.setText(edittextinput);
+            }
+        });
+
+
     }
 
     @Override
@@ -59,7 +71,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-     /*textView.setText(edittextinput);*/
-
 
 }
