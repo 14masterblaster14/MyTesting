550fcf3c2129272c8074facce8cd86c364c94f5b
comp <14masterblaster14@gmail.com>
2016-12-07 23:53:27 +0530

    Dir Commit

--- 11.2snackbar/src/main/java/com/example/a112snackbar/MainActivity.java
+++ 11.2snackbar/src/main/java/com/example/a112snackbar/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a112snackbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +33,46 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // My code starts here --
+
+        final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        final CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Snackbar snackbar = Snackbar
+                            .make(coordinatorLayout, "Checkbox is Checked", Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+
+                                    checkBox.setChecked(false);
+                                    Snackbar snackbar1 = Snackbar
+                                            .make(coordinatorLayout, "Checkbox is Unchecked", Snackbar.LENGTH_LONG);
+                                    snackbar1.show();
+                                }
+                            });
+                    // Changing Action message text color
+                    snackbar.setActionTextColor(Color.BLUE);
+
+                    // Changing snackbar background color
+                    View snackbarView = snackbar.getView();
+                    snackbarView.setBackgroundColor(Color.YELLOW);
+
+                    // Changing snackbar message color
+                    TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                    textView.setTextColor(Color.GREEN);
+                    snackbar.show();
+                }
+            }
+        });
 
         // My code ends here --
     }
