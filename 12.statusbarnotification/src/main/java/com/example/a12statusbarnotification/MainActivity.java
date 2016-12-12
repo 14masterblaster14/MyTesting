238a574d4ecdf7c3fbfa60cdc8b4f4d3f0ef5d26
comp <14masterblaster14@gmail.com>
2016-12-12 14:27:33 +0530

    Dir Commit

--- 12.statusbarnotification/src/main/java/com/example/a12statusbarnotification/MainActivity.java
+++ 12.statusbarnotification/src/main/java/com/example/a12statusbarnotification/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.a12statusbarnotification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 
 /* INCLUDE Notification activity in Manifest file */
@@ -13,6 +17,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.Btn_Notify).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showNotification();
+            }
+        });
+
+    }
+
+    private void showNotification() {
+
+        Intent intent =
+                new Intent(this, NotificationActivity.class);
+
+        PendingIntent contentIntent =
+                PendingIntent.getActivity(this,
+                        1234,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder
+                = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                .setContentTitle("Title")
+                .setContentInfo("Info")
+                .setContentText("Text")
+                .setContentIntent(contentIntent)
+                .setTicker("Ticker")
+                .setDefaults(Notification.DEFAULT_ALL)
+                .addAction(R.mipmap.ic_launcher, "Reply", contentIntent)
+                .setSmallIcon(R.mipmap.ic_launcher);
+
+        Notification notification = builder.build();
+        NotificationManager manager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(1222, notification);
 
     }
 }
--- 13.dialogotification/src/main/java/com/example/a13dialogotification/MainActivity.java
+++ 13.dialogotification/src/main/java/com/example/a13dialogotification/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.Btn_Alert).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showDialog(MyDialogFragment.TAG_ALERT);
+            }
+        });
+
+        findViewById(R.id.Btn_DatePicker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(MyDialogFragment.TAG_DATE_PICKER);
+            }
+        });
+
+        findViewById(R.id.Btn_TimePicker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(MyDialogFragment.TAG_TIME_PICKER);
+            }
+        });
+
+        findViewById(R.id.Btn_Progress).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(MyDialogFragment.TAG_PROGRESS_DIALOG);
+            }
+        });
+
+        findViewById(R.id.Btn_Custom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(MyDialogFragment.TAG_CUSTOM);
+            }
+        });
+
+
+      /*  findViewById(R.id.SelectCountry).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RadioButton radioButtonAmerica = (RadioButton) findViewById(R.id.America);
+                showToast(radioButtonAmerica.isChecked() ? "America" : "China");
+           }
+        });  */
+
+      /*
+        RadioButton radioButtonAmerica = (RadioButton) findViewById(R.id.America);
+        showToast(radioButtonAmerica.isChecked() ? "America" : "China");
+        */
+    }
+
+    private void showDialog(String tag) {
+
+        MyDialogFragment myDialogFragment = new MyDialogFragment();
+        myDialogFragment.show(getSupportFragmentManager(), tag);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
