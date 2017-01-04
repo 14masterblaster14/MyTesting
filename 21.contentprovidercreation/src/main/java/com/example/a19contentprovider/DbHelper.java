8c381e5fd4e0aa36e860f4c6aac0c9cb202c9465
comp <14masterblaster14@gmail.com>
2017-01-04 08:17:12 +0530

    Dir Commit

--- 21.contentprovidercreation/src/main/java/com/example/a19contentprovider/DbHelper.java
+++ 21.contentprovidercreation/src/main/java/com/example/a19contentprovider/DbHelper.java
@@ -1,9 +1,11 @@
+package com.example.a19contentprovider;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by comp on 12/29/2016.
+ * Created by comp on 1/3/2017.
  */
 
 public class DbHelper extends SQLiteOpenHelper {
@@ -14,11 +16,13 @@ public class DbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        // This method will be called only once.
+        db.execSQL("Create table UserTable (UserName text, RollNo number Primary key asc )");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        // This method will be called only if the "version" value is different in DbHelpers constructor .
     }
 }
